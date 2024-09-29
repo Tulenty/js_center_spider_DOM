ba7094f2f3cdb088ee367521f1e3c14658f4124e
missk <rostock.vasiljev@gmail.com>
2024-09-29 16:55:40 +0300

    make build

---
+++ index.df7aa38f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.top="".concat((t.clientWidth-e.clientWidth)/2,"px"),e.style.left="".concat((t.clientHeight-e.clientHeight)/2,"px");
+//# sourceMappingURL=index.df7aa38f.js.map
